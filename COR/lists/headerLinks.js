d58704958740f3acc768feed31366a1fbc919496
PhantomLuigi <PhantomLuigi08@gmail.com>
2023-07-27 11:04:08 +1000

    Delete COR/lists directory

--- COR/lists/headerLinks.js
+++
@@ -1,5 +0,0 @@
-var headerLinks = {
-    "1": [["info", "info.html"],["inventories", "inventories.html"],["stats", "stats.html"]],
-    "2": [["info", "info.html"],["inventories", "inventories.html"],["stats", "stats.html"]],
-    "3": [["info", "info.html"],["inventories", "inventories.html"],["stats", "stats.html"],["buildings", "buildings.html"],["jobs","job.html"],["living-things","livingthings.html"],["contact list","contactlist.html"],["city map","citymap.html"]],
-}
